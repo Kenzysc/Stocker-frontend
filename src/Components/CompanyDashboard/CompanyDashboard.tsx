7e5a41bb49fce2f82bbbb10a59641670254ba7e5
Kenzysc <kenzysc@gmail.com>
2024-05-13 23:05:03 +0100

    Add UseEffect

---
+++ src/Components/CompanyDashboard/CompanyDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {}
+
+const CompanyDashboard = (props: Props) => {
+  return (
+    <div>CompanyDashboard</div>
+  )
+}
+
+export default CompanyDashboard
